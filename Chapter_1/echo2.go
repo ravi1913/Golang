bb3295b10a3571d7acec71cada1a954354a6590d
ravi1913 <ravi.teja9991516@gmail.com>
2021-04-26 19:00:23 +0530

    echo2.go and echo3.go changed to include processing time.
    It is observed that for the same arguments, the "Join" function is marginally faster than the loop.

--- Chapter_1/echo2.go
+++ Chapter_1/echo2.go
@@ -6,13 +6,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	s, sep := "", ""
 	for _, arg := range os.Args[1:] {
 		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
+	fmt.Printf("%.8fs elapsed\n", time.Since(start).Seconds())
 }
--- Chapter_1/echo3.go
+++ Chapter_1/echo3.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Printf("%.8fs elapsed\n", time.Since(start).Seconds())
 }
