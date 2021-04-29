eda1588ffdb51542ca04052d58c0d1bf4066bf0c
ravi1913 <ravi.teja9991516@gmail.com>
2021-04-29 12:07:05 +0530

    Updated lissajous to generate random colors in the output.

--- Chapter_1/scripts/lissajous.go
+++ Chapter_1/scripts/lissajous.go
@@ -9,19 +9,42 @@ import (
 	"math"
 	"math/rand"
 	"os"
+	"time"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{
+	color.White,
+	color.Black,
+	color.RGBA{0x4f, 0xeb, 0x34, 0xff},
+	color.RGBA{0xeb, 0x49, 0x34, 0xff},
+	color.RGBA{0xeb, 0xe1, 0x34, 0xff},
+	color.RGBA{0x34, 0xeb, 0xcc, 0xff},
+	color.RGBA{0x34, 0xc6, 0xeb, 0xff},
+	color.RGBA{0x34, 0x3a, 0xeb, 0xff},
+	color.RGBA{0x89, 0x34, 0xeb, 0xff},
+	color.RGBA{0xeb, 0x34, 0xcf, 0xff},
+}
 
 const (
-	whiteIndex = 0
-	blackIndex = 1
+	whiteIndex  = 0
+	blackIndex  = 1
+	greenIndex  = 2
+	redIndex    = 3
+	yellowIndex = 4
+	cyanIndex   = 5
+	blueIndex   = 6
+	indigoIndex = 7
+	purpleIndex = 8
+	pinkIndex   = 9
 )
 
 func main() {
-	lissajous(os.Stdout)
+	rand.Seed(time.Now().UnixNano())
+	n := 1 + rand.Intn(9-0+1)
+	newIndex := uint8(n)
+	lissajous(os.Stdout, newIndex)
 }
-func lissajous(out io.Writer) {
+func lissajous(out io.Writer, index uint8) {
 	const (
 		cycles  = 5
 		res     = 0.001
@@ -39,7 +62,7 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), index)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
